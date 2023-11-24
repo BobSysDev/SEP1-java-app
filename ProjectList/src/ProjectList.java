57d829d9d42048aa8463a4495f7177f500c14c77
Marius Gafton <mariusgafton2005@gmail.com>
2023-11-24 23:01:05 +0100

    ProjectList

---
+++ ProjectList/src/ProjectList.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProjectList<Project> {
+    private List<Project> projects;
+
+    public ProjectList() {
+        projects = new ArrayList<>();
+    }
+
+    public void addProject(Project project) {
+        projects.add(project);
+    }
+
+    public void removeProject(Project project) {
+        projects.remove(project);
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+}
