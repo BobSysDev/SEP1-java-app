21d60f9051fe9915d62c3e4a068d825620628c98
Karolina Krysiak <71433414+KarolinaKrysiak@users.noreply.github.com>
2023-11-26 18:44:11 +0100

    🎉 Customer classes + phone
    Set the empty customer classes + made phone class

---
+++ Customer/src/Phone.java
@@ -0,0 +1,39 @@
+public class Phone {
+    private String countryCode;
+    private String phoneNumber;
+
+    public Phone(String countryCode, String phoneNumber) {
+        setCountryCode(countryCode);
+        setPhoneNumber(phoneNumber);
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if (phoneNumber.length() < 4 || phoneNumber.length() > 13) {
+            throw new IllegalArgumentException("Invalid phone number");
+        }
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFullPhoneNumber() {
+        return countryCode + phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone: " + getFullPhoneNumber();
+    }
+
+
+}
