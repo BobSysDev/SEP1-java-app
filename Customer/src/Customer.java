ba10a75ae2cca8292846a9b0441af74f373c3c42
Karolina Krysiak <71433414+KarolinaKrysiak@users.noreply.github.com>
2023-11-27 14:47:41 +0100

    ✨ Instance variables in Customer
    Added instance variables domming from associated classes to the Customer class

--- Customer/src/Customer.java
+++ Customer/src/Customer.java
@@ -1,8 +1,18 @@
 public class Customer {
     private boolean isPrivate;
+    private Name name;
+    private Phone phone;
+    private Email email;
+    private Company company;
 
-    public Customer(boolean isPrivate) {
-        setPrivate(isPrivate);
+    public Customer(boolean isPrivate, Name name, Phone phone, Email email, Company company) {
+        this.isPrivate = isPrivate;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        if (!isPrivate) {
+            this.company = company;
+        }
     }
 
     public boolean isPrivate() {
@@ -11,5 +21,44 @@ public class Customer {
 
     public void setPrivate(boolean isPrivate) {
         this.isPrivate = isPrivate;
+        if (isPrivate) {
+            this.company = null;
+        }
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    public Phone getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Phone phone) {
+        this.phone = phone;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public void
+
+    setEmail(Email email) {
+        this.email = email;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        if (!isPrivate) {
+            this.company = company;
+        }
     }
 }
