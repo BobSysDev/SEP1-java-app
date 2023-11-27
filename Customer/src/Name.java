34f01fd07ada6382b1a816dd66c3c8b29365f25b
Karolina Krysiak <71433414+KarolinaKrysiak@users.noreply.github.com>
2023-11-27 01:00:21 +0100

    🔨 Name class content
    Basic Name class functionality

--- Customer/src/Name.java
+++ Customer/src/Name.java
@@ -1,2 +1,61 @@
 public class Name {
+
+    private String firstName;
+    private String middleName;
+    private String lastName;
+
+    public Name(String firstName, String lastName) {
+        setFirstName(firstName);
+        setMiddleName(middleName);
+        setLastName(lastName);
+
+    }
+
+    public Name(String firstName, String middleName, String lastName) {
+        this(firstName, lastName);
+        this.middleName = middleName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        if (firstName == null || firstName.isEmpty()) {
+            throw new IllegalArgumentException("First name cannot be empty");
+        }
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = "";
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName == null || lastName.isEmpty()) {
+            throw new IllegalArgumentException("Last name cannot be empty");
+        }
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        if (middleName.isEmpty()) {
+            return firstName + " " + lastName;
+        } else {
+            return firstName + " " + middleName + " " + lastName;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + firstName + " " + middleName + " " + lastName;
+    }
 }
